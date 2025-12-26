2d9375b96c5eb1cadd7c4034693c4eabc560c021
Aditya <adityawarekar1984@gmail.com>
2025-12-26 15:08:13 +0530

    ADDing and verfiying token i console

--- base/views.py
+++ base/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+from agora_token_builder import RtcTokenBuilder
 from django.http import JsonResponse
 from django.conf import settings
 import time
+import random
 
-from agora_token_builder import RtcTokenBuilder
+def geToken(request):
+    appId = '05895611c83b4515b2b8dc0dfe05f3b5'
+    appCertificate = '1e7583e448484d40a66aa77541e8ddd1'
+    channelName = request.GET.get('channel')
+    uid = random.randint(1,230)
+    expirationTimeInSeconds = 3600 * 24
+    currentTimeStamp = time.time()
+    privilegeExpiredTs = currentTimeStamp + expirationTimeInSeconds
+    role = 1
 
+    token = RtcTokenBuilder.buildTokenWithUid(appId, appCertificate, channelName, uid, role, privilegeExpiredTs)
+    return JsonResponse({'token':token, 'uid':uid}, safe=False)
 def index(request):
     return render(request, 'base/lobby.html')
 
--- my_chat/static/js/streams.js
+++ my_chat/static/js/streams.js
@@ -1,54 +1,110 @@
 const APP_ID = '05895611c83b4515b2b8dc0dfe05f3b5'
-const CHANNEL = 'main'
-const TOKEN = '007eJxTYNj1Zqdv5r7p611i4+QbhHddTJX6bLJUpVIu/dxzGb4Jm60VGAxMLSxNzQwNky2Mk0xMDU2TjJIsUpINUtJSDUzTjJNM7RZ5ZzYEMjJwJE5mYWSAQBCfhSE3MTOPgQEACacd0A=='
-
+const CHANNEL = 'sessionStorage.getItem('room')
+const TOKEN = '00605895611c83b4515b2b8dc0dfe05f3b5IACrd5HuHu8JfYUtgbmabTCaVTTxLuDKQ8qaPRL0Yj+V42f+nQiGzqzzIgC4kS0Ecn9PaQQAAQByf09pAgByf09pAwByf09pBAByf09p'
+let UID = 29;
 const client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' })
 
 let localTracks = []
 let remoteUsers = {}
 let UID = null
 
-const joinAndDisplayLocalStream = async () => {
-    UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
+let joinAndDisplayLocalStream = async () => {
+    document.getElementById('room-name').innerText = CHANNEL
 
+    client.on('user-published', handleUserJoined)
+    client.on('user-left', handleUserLeft)
+
+    try{
+        UID = await client.join(APP_ID, CHANNEL, TOKEN, UID)
+    }catch(error){
+        console.error(error)
+        window.open('/', '_self')
+    }
+    
     localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
 
-    const player = `
-        <div class="video-container" id="user-container-${UID}">
-            <div class="video-player" id="user-${UID}"></div>
-        </div>
-    `
-    document.getElementById('video-streams')
-        .insertAdjacentHTML('beforeend', player)
+    let member = await createMember()
 
+    let player = `<div  class="video-container" id="user-container-${UID}">
+                     <div class="video-player" id="user-${UID}"></div>
+                     <div class="username-wrapper"><span class="user-name">${member.name}</span></div>
+                  </div>`
+    
+    document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
     localTracks[1].play(`user-${UID}`)
-
-    await client.publish(localTracks)
+    await client.publish([localTracks[0], localTracks[1]])
 }
 
-// JOIN BUTTON CLICK (VERY IMPORTANT)
-document.getElementById('join-btn').addEventListener('click', () => {
-    joinAndDisplayLocalStream()
-})
-
-// REMOTE USERS
-client.on('user-published', async (user, mediaType) => {
+let handleUserJoined = async (user, mediaType) => {
     remoteUsers[user.uid] = user
     await client.subscribe(user, mediaType)
 
-    if (mediaType === 'video') {
-        const player = `
-            <div class="video-container" id="user-container-${user.uid}">
-                <div class="video-player" id="user-${user.uid}"></div>
-            </div>
-        `
-        document.getElementById('video-streams')
-            .insertAdjacentHTML('beforeend', player)
+    if (mediaType === 'video'){
+        let player = document.getElementById(`user-container-${user.uid}`)
+        if (player != null){
+            player.remove()
+        }
 
+        let member = await getMember(user)
+
+        player = `<div  class="video-container" id="user-container-${user.uid}">
+            <div class="video-player" id="user-${user.uid}"></div>
+            <div class="username-wrapper"><span class="user-name">${member.name}</span></div>
+        </div>`
+
+        document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
         user.videoTrack.play(`user-${user.uid}`)
     }
 
-    if (mediaType === 'audio') {
+    if (mediaType === 'audio'){
         user.audioTrack.play()
     }
-})
+}
+
+let handleUserLeft = async (user) => {
+    delete remoteUsers[user.uid]
+    document.getElementById(`user-container-${user.uid}`).remove()
+}
+
+let leaveAndRemoveLocalStream = async () => {
+    for(let i=0; localTracks.length > i; i++){
+        localTracks[i].stop()
+        localTracks[i].close()
+
+    }
+    await client.leave()
+    window.open('/','_self')
+}
+
+let toggleCamera = async (e) => {
+    if(localTracks[1].muted){
+        await localTracks[1].setMuted(false)
+        e.target.style.backgroundColor = '#fff'
+
+    }else{
+        await localTracks[1].setMuted(true)
+        e.target.style.backgroundColor = 'rgb(255, 80, 80, 1)'
+    }
+}
+
+let toggleMic = async (e) => {
+    if(localTracks[0].muted){
+        await localTracks[0].setMuted(false)
+        e.target.style.backgroundColor = '#fff'
+
+    }else{
+        await localTracks[0].setMuted(true)
+        e.target.style.backgroundColor = 'rgb(255, 80, 80, 1)'
+    }
+
+}
+
+
+joinAndDisplayLocalStream()
+document.getElementById('leave-btn').addEventListener('click', leaveAndRemoveLocalStream)
+document.getElementById('camera-btn').addEventListener('click', toggleCamera)
+document.getElementById('mic-btn').addEventListener('click', toggleCamera)
+
+
+
+
